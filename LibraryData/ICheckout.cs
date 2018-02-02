5054318da96c3dbd6a36e1f625b0ab8195979ec7
Sandeera Jayampathi <sandeerads@gmail.com>
2018-02-02 14:30:12 +0530

    implemented checkoutservices

---
+++ LibraryData/ICheckout.cs
@@ -0,0 +1,30 @@
+﻿using LibraryData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface ICheckout
+    {
+        IEnumerable<Checkouts> GetAll();
+        Checkouts Get(int id);
+        void Add(Checkouts newCheckout);
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int id);
+        void PlaceHold(int assetId, int libraryCardId);
+        void CheckoutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId);
+        Checkouts GetLatestCheckout(int id);
+        int GetNumberOfCopies(int id);
+        int GetAvailableCopies(int id);
+        bool IsCheckedOut(int id);
+
+        string GetCurrentHoldPatron(int id);
+        string GetCurrentHoldPlaced(int id);
+        string GetCurrentPatron(int id);
+        IEnumerable<Holds> GetCurrentHolds(int id);
+
+        void MarkLost(int id);
+        void MarkFound(int id);
+    }
+}
